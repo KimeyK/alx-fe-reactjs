234c324124307dddc01b8135e756706c020a2b7c
Simon Kimani <simonkaranja8@gmail.com>
2025-09-13 21:55:26 +0300

    feat(alx-react-app-new): inline CSS in Header, MainContent, Footer, and UserProfile

---
+++ alx-react-app-new/src/MainContent.jsx
@@ -0,0 +1,11 @@
+﻿function MainContent() {
+  return (
+    <main style={{ backgroundColor: '#f9f9f9', padding: '15px', margin: '10px', borderRadius: '8px' }}>
+      <p style={{ fontSize: '18px', color: '#333' }}>
+        I love to visit New York, Paris, and Tokyo.
+      </p>
+    </main>
+  );
+}
+
+export default MainContent;
