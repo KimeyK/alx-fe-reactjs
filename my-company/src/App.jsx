aff719ed0f94bfacdc057750d6755c793d9b2641
Simon Kimani <simonkaranja8@gmail.com>
2025-09-14 15:32:26 +0300

    feat(my-company): add React Router, pages (Home/About/Services/Contact), Navbar, inline styles, and contact form

---
+++ my-company/src/App.jsx
@@ -0,0 +1,25 @@
+﻿import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./Navbar";
+import Home from "./Home";
+import About from "./About";
+import Services from "./Services";
+import Contact from "./Contact";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div style={{ fontFamily: "sans-serif" }}>
+        <Navbar />
+        <div style={{ maxWidth: "900px", margin: "0 auto" }}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/services" element={<Services />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
+  );
+}
+export default App;
---
+++ my-company/src/Navbar.jsx
@@ -0,0 +1,22 @@
+﻿import { Link } from "react-router-dom";
+
+function Navbar() {
+  const navStyle = {
+    display: "flex",
+    gap: "16px",
+    padding: "12px 20px",
+    backgroundColor: "#222",
+    color: "white",
+  };
+  const linkStyle = { color: "white", textDecoration: "none" };
+
+  return (
+    <nav style={navStyle}>
+      <Link to="/" style={linkStyle}>Home</Link>
+      <Link to="/about" style={linkStyle}>About</Link>
+      <Link to="/services" style={linkStyle}>Services</Link>
+      <Link to="/contact" style={linkStyle}>Contact</Link>
+    </nav>
+  );
+}
+export default Navbar;
