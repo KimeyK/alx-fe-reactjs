00d0190e4c6e5c95699d4456b61b0cac5196261e
Simon Kimani <99262824+KimeyK@users.noreply.github.com>
2025-09-21 17:02:35 +0300

    Update main.jsx

--- github-user-search/src/main.jsx
+++ github-user-search/src/main.jsx
@@ -1,10 +1,1 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'   // <-- this line must be present
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import './index.css'
