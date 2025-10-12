e59abbacffbae109ad6a420a8248257ff965ffe2
Simon Kimani <99262824+KimeyK@users.noreply.github.com>
2025-10-12 23:04:43 +0300

    Create Home.jsx

---
+++ react-router-advanced/src/components/Home.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <section>
+      <h1>Home</h1>
+      <p>Welcome! Try the Blog, a dynamic Post, or the protected Profile.</p>
+    </section>
+  );
+}
