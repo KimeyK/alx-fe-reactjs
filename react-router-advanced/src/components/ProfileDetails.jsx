ea4cee04988376efdd8f6046298bf3a2db0899d9
Simon Kimani <99262824+KimeyK@users.noreply.github.com>
2025-10-12 23:19:50 +0300

    Create ProfileDetails.jsx

---
+++ react-router-advanced/src/components/ProfileDetails.jsx
@@ -0,0 +1,9 @@
+export default function ProfileDetails() {
+  return (
+    <div>
+      <h2>Profile Details</h2>
+      <p>Name: Jane Doe</p>
+      <p>Email: jane@example.com</p>
+    </div>
+  );
+}
