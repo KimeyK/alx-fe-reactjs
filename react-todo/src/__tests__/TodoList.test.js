ceee4e4072210b8b09676f37b838736806d6b2e6
Simon Kimani <99262824+KimeyK@users.noreply.github.com>
2025-10-16 19:31:28 +0300

    Update and rename TodoList.test.jsx to TodoList.test.js

---
+++ react-todo/src/__tests__/TodoList.test.js
@@ -0,0 +1,40 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import TodoList from "../components/TodoList";
+
+function addTodo(text) {
+  const input = screen.getByLabelText(/todo-input/i);
+  fireEvent.change(input, { target: { value: text } });
+  fireEvent.submit(screen.getByRole("form", { name: /add-todo-form/i }));
+}
+
+describe("TodoList", () => {
+  test("renders initial todos", () => {
+    render(<TodoList />);
+    expect(screen.getByText(/learn react/i)).toBeInTheDocument();
+    expect(screen.getByText(/write tests/i)).toBeInTheDocument();
+  });
+
+  test("adds a new todo", () => {
+    render(<TodoList />);
+    addTodo("Buy milk");
+    expect(screen.getByText(/buy milk/i)).toBeInTheDocument();
+  });
+
+  test("toggles a todo", () => {
+    render(<TodoList />);
+    const todo = screen.getByText(/learn react/i);
+    expect(todo).toHaveStyle({ textDecoration: "none" });
+    fireEvent.click(todo);
+    expect(todo).toHaveStyle({ textDecoration: "line-through" });
+  });
+
+  test("deletes a todo", () => {
+    render(<TodoList />);
+    const todo = screen.getByText(/write tests/i);
+    const li = todo.closest("li");
+    const del = li.querySelector("button");
+    fireEvent.click(del);
+    expect(screen.queryByText(/write tests/i)).not.toBeInTheDocument();
+  });
+});
